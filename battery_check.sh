18a94e0c49f91e4557b25e6094d3968924917f2d
Jiří Nádvorník <nadvornik.jiri@gmail.com>
2017-08-05 15:16:48 +0200

    change in treshold for battery life from development to real value

--- battery_check.sh
+++ battery_check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 batperc=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep 'percentage' | tr -d -c 0-9`
-if [[ $batperc < 82 ]] ; then
+if [[ $batperc < 15 ]] ; then
     DISPLAY=:0.0 /usr/bin/notify-send "low battery" "$BATTINFO"
 fi
