22a51dae8057cb7d51a00d290b1bc23b732b7d58
Jiří Nádvorník <nadvornik.jiri@gmail.com>
2018-09-24 11:21:09 +0200

    +y/n confirmation in unblock

--- unblock_communications.sh
+++ unblock_communications.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
-echo "Please wait 5 seconds. Are you sure you cannot avoid sending messages?"
+echo "Please wait 5 seconds. Are you sure you cannot avoid checking messages?"
 sleep 5
-echo "Are you sure? (y/n)"
+while true; do
+    read -p "Are you sure (y/n)? " yn
+    case $yn in
+        [Yy]* ) break;;
+        * ) exit;;
+    esac
+done
+
+echo "I will block you for 30 seconds"
 sleep 30
 echo "Ok, here we go"
 sudo sed -i -r '/BLOCKME/s/^/# /g' /etc/hosts
