e2367dfaa76cc316eac1372b2bd6a3d224ab4af4
Jiří Nádvorník <nadvornik.jiri@gmail.com>
2018-09-25 08:56:03 +0200

    + ctrl-c trap

--- unblock_communications.sh
+++ unblock_communications.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -e
+
+int_handler()
+{
+    echo "Interrupted. Good choice."
+    # Kill the parent process of the script.
+    kill $PPID
+    exit 1
+}
+trap 'int_handler' INT
+
 echo "Please wait 5 seconds. Are you sure you cannot avoid checking messages?"
 sleep 5
 while true; do
@@ -10,7 +20,7 @@ while true; do
     esac
 done
 
-echo "I will block you for 30 seconds"
+echo "I will block you for 30 more seconds, you can still interrupt it."
 sleep 30
-echo "Ok, here we go"
+echo "ok, unblocked"
 sudo sed -i -r '/BLOCKME/s/^/# /g' /etc/hosts
